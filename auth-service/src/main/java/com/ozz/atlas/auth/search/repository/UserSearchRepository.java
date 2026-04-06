f8e82297372a376d74921c48f1ca1a8f9ee29a12
kimdogyun <sygd1214@gmail.com>
2026-04-06 18:26:07 +0900

    사용자 검색 ES 적용

--- auth-service/src/main/java/com/ozz/atlas/auth/search/repository/UserSearchRepository.java
+++ auth-service/src/main/java/com/ozz/atlas/auth/search/repository/UserSearchRepository.java
@@ -1,7 +1,10 @@
 package com.ozz.atlas.auth.search.repository;
 
+import com.ozz.atlas.auth.domain.User;
 import com.ozz.atlas.auth.search.document.UserDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 public interface UserSearchRepository extends ElasticsearchRepository<UserDocument, Long> {
+
 }
