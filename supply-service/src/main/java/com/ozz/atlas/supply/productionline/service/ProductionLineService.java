b36a7b7dc000ad60f4359dbd8b0c4fb41d41dd91
kimdogyun <sygd1214@gmail.com>
2026-04-07 10:09:17 +0900

    생산라인 목록 및 상세 조회 기능 추가

--- supply-service/src/main/java/com/ozz/atlas/supply/productionline/service/ProductionLineService.java
+++ supply-service/src/main/java/com/ozz/atlas/supply/productionline/service/ProductionLineService.java
@@ -7,6 +7,8 @@ import com.ozz.atlas.supply.productionline.dtos.ProductionLineCreateDto;
 import com.ozz.atlas.supply.productionline.dtos.ProductionLineResponseDto;
 import com.ozz.atlas.supply.productionline.repository.ProductionLineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,5 +42,22 @@ public class ProductionLineService {
 
         ProductionLine productionLine = productionLineRepository.save(dto.toEntity());
         return ProductionLineResponseDto.fromEntity(productionLine);
+    }
+
+    //    생산라인 목록 조회
+    public Page<ProductionLineResponseDto> productionLines(Pageable pageable) {
+        return productionLineRepository.findAllByStatus(Status.ACTIVE, pageable)
+                .map(ProductionLineResponseDto::fromEntity);
+
+    }
+
+    //    생산라인 상세 조회
+    public ProductionLineResponseDto productionLIne(Long productionLineId) {
+        ProductionLine productionLine = productionLineRepository
+                .findByProductionLineIdAndStatus(productionLineId, Status.ACTIVE)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 생산라인 입니다"));
+
+        return ProductionLineResponseDto.fromEntity(productionLine);
+
     }
 }
