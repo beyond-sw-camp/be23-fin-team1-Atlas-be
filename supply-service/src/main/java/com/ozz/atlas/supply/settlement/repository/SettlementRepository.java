88e10962c6072ff7cc086d4c1e0203fc599ace88
ybrocks <ybrocks@naver.com>
2026-04-16 12:15:38 +0900

    정산 목록조회 page방식으로 변경

--- supply-service/src/main/java/com/ozz/atlas/supply/settlement/repository/SettlementRepository.java
+++ supply-service/src/main/java/com/ozz/atlas/supply/settlement/repository/SettlementRepository.java
@@ -5,11 +5,7 @@ import com.ozz.atlas.supply.settlement.domain.SettlementStatus;
 import com.ozz.atlas.supply.settlement.domain.SettlementTargetType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface SettlementRepository extends JpaRepository<Settlement, Long> {
-
-    List<Settlement> findAllByOrderByIdDesc();
 
 //    정산 중복 체크
     boolean existsByTargetTypeAndTargetPublicIdAndSettlementStatusNot(
