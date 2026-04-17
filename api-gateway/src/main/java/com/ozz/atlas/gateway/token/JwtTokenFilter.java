263b7701bdeb2ba5d4bc29caccc39a4b9eb8864e
2001056 <lbchan1027@gmail.com>
2026-04-17 14:47:13 +0900

    웹소켓(STOMP) 연결을 위한 JWT 인증 필터 예외 경로 추가

--- api-gateway/src/main/java/com/ozz/atlas/gateway/token/JwtTokenFilter.java
+++ api-gateway/src/main/java/com/ozz/atlas/gateway/token/JwtTokenFilter.java
@@ -37,7 +37,7 @@ public class JwtTokenFilter implements GlobalFilter {
                 || (HttpMethod.POST.equals(method) && "/api/auth/login".equals(urlPath))
                 || (HttpMethod.POST.equals(method) && "/api/auth/users".equals(urlPath))
                 || (HttpMethod.POST.equals(method) && "/api/auth/refresh".equals(urlPath))
-                || urlPath.startsWith("/ws-chat")) {
+                || urlPath.startsWith("api/control/ws-chat")) {
             return chain.filter(exchange);
         }
         try {
