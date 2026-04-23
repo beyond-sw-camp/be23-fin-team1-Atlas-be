13a929e25583c6f860ef877a86489581b6c88937
yune22 <wjddbs020604@gmail.com>
2026-04-23 16:14:51 +0900

    ShipmentRepository 하위발주 ID 목록 조회 메서드 추가

--- supply-service/src/main/java/com/ozz/atlas/supply/shipment/repository/ShipmentRepository.java
+++ supply-service/src/main/java/com/ozz/atlas/supply/shipment/repository/ShipmentRepository.java
@@ -3,9 +3,14 @@ package com.ozz.atlas.supply.shipment.repository;
 import com.ozz.atlas.supply.shipment.domain.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
 
     Optional<Shipment> findByPublicId(String publicId);
+
+    List<Shipment> findAllBySubPoIdIn(Collection<Long> subPoIds);
+
 }
